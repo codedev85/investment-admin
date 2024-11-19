5b42dd84b60cdf9746c4ed52631d638e69138555
Olawuyi <codedev85@gmail.com>
2024-11-19 09:53:53 +0100

    add super user active

--- auth/settings.py
+++ auth/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['https://investmentui.veloxsolution.ng/', 'investmentui.veloxsolution.ng/']
+ALLOWED_HOSTS = ['*']
 
 # Load environment variables from .env file
 load_dotenv()
@@ -150,27 +150,9 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://investmentui.veloxsolution.ng/",  # React frontend URL
-#     "https://investment.veloxsolution.ng/",
-#     "investmentui.veloxsolution.ng/",  # Backend URL
-#     ".veloxsolution.ng",
-#     'https//*.veloxsolution.ng',
-#     "*.veloxsolution.ng",
-# ]
+CSRF_TRUSTED_ORIGINS = ['https://investmentui.veloxsolution.ng/']
 
-CSRF_TRUSTED_ORIGINS = ['https://investmentui.veloxsolution.ng']
-
-CORS_ALLOWED_ORIGINS = ['https://investmentui.veloxsolution.ng']
-
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",  # LocalFrontEnd Url
-#     "https://investmentui.veloxsolution.ng/",
-#     "investmentui.veloxsolution.ng/",
-#     'https//*.veloxsolution.ng',
-#     "*.veloxsolution.ng",
-#     ".veloxsolution.ng",
-# ]
+CORS_ALLOWED_ORIGINS = ['https://investmentui.veloxsolution.ng/']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
